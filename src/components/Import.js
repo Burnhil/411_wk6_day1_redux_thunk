aff3effc981134dc8fcab0275f786ca4e3bdb213
Burnhil <Royal42Crown@gmail.com>
2020-07-16 16:08:09 -0500

    still unable to remove line delete working on count

--- src/components/Import.js
+++ src/components/Import.js
@@ -1,10 +1,67 @@
 import React from 'react'
+import { Button, Container, IconButton, Menu, MenuItem, Table, TableHead, TableBody, TableRow, TableCell } from '@material-ui/core'
+import DeleteIcon from '@material-ui/icons/Delete'
+import { MoreVert } from '@material-ui/icons'
+
 
 const Import = (props) => {
     // fill out this component
 
+    const [anchorEl, setAnchorEl] = React.useState(null);
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
     return (
-        <p>Import Component</p>
+        <Container>
+            <Button variant="contained" color="primary" onClick={props.fetchMakes}>Import</Button>
+            <h2>COUNT: {props.makes.length}</h2>
+            <Table>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Id</TableCell>
+                        <TableCell>Make</TableCell>
+                        <TableCell>Actions</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {props.makes.map((row, index) => (
+                        <TableRow key={row.MakeId}>
+                            <TableCell>{row["MakeId"]}</TableCell>
+                            <TableCell>{row["MakeName"]}</TableCell>
+                            <TableCell>
+                                
+                                
+                                <IconButton
+										helvetica-label="more"
+										helvetica-controls="long-menu"
+										helvetica-haspopup="true"
+										onClick={handleClick}>
+										<MoreVert />
+								</IconButton>
+                                <Menu
+                                    id="simple-menu"
+                                    anchorEl={anchorEl}
+                                    keepMounted
+                                    open={Boolean(anchorEl)}
+                                    onClose={handleClose}
+                                >
+                                    <MenuItem onClick={() => props.deleteMake(index)}>Delete</MenuItem>
+                    
+                                </Menu>
+                            </TableCell>
+
+                        </TableRow>
+                    ))}
+
+                </TableBody>
+            </Table>
+        </Container>
     )
 }
 
--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,5 +1,27 @@
 const url = "https://vpic.nhtsa.dot.gov/api/vehicles/GetMakesForVehicleType/car?format=json"
 
+export const deleteMake = (index) => {
+    console.log("inside delete make");
+    return {
+        type: 'DELETE_MAKE',
+        value: index
+    }
+}
+
+export const fetchMakes = () => {
+    return (dispatch) => {
+        fetch("https://vpic.nhtsa.dot.gov/api/vehicles/GetMakesForVehicleType/car?format=json")
+            .then(res => res.json())
+            .then(response => {
+                const action = {
+                    type: 'FETCH_MAKES',
+                    value: response.Results
+                }
+                dispatch(action)
+            })
+    }
+}
+
 export const addCar = (car) => {
     return {
         type: 'ADD_CAR',
